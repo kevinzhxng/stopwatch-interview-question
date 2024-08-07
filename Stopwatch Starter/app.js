c60b3c79c88b415bcc1e64fab9c1b2041bb48631
Kevin Zhang <kzhang0826@gmail.com>
2024-08-07 18:41:55 -0400

    fixed stop timer being pressed twice doubles timer

--- Stopwatch Starter/app.js
+++ Stopwatch Starter/app.js
@@ -2,6 +2,9 @@ const timerMilliseconds = document.querySelector(".timer__milliseconds");
 const timerSeconds = document.querySelector(".timer__seconds");
 const timerMinutes = document.querySelector(".timer__minutes");
 
+let start;
+let stopTimers = (document.querySelector(".stopwatch__stop").disabled = true);
+
 let cancelId;
 let startTime;
 let savedTime = 0;
@@ -10,18 +13,22 @@ function startTimer() {
   startTime = Date.now();
   console.log(startTime);
   cancelId = requestAnimationFrame(updateTimer);
+  start = (document.querySelector(".stopwatch__start").disabled = true);
+  stopTimers = (document.querySelector(".stopwatch__stop").disabled = false);
 }
 
 function stopTimer() {
-    savedTime = savedTime + Date.now() - startTime
-    console.log(savedTime)
+  savedTime += Date.now() - startTime;
+  console.log(savedTime);
   cancelAnimationFrame(cancelId);
+  start = (document.querySelector(".stopwatch__start").disabled = false);
+  stopTimers = (document.querySelector(".stopwatch__stop").disabled = true);;
 }
 
 function resetTimer() {
-  timerMilliseconds.innerHTML = '000'
-  timerSeconds.innerHTML = '00'
-  timerMinutes.innerHTML = '00'
+  timerMilliseconds.innerHTML = "000";
+  timerSeconds.innerHTML = "00";
+  timerMinutes.innerHTML = "00";
   savedTime = 0;
   startTime = Date.now();
 }
@@ -30,7 +37,7 @@ function updateTimer() {
   let millisElapsed = savedTime + (Date.now() - startTime);
   let millisecondsText = millisElapsed % 1000;
   if (millisecondsText.toString().length < 3) {
-    millisecondsText = millisecondsText.toString().padStart(3, '0');
+    millisecondsText = millisecondsText.toString().padStart(3, "0");
   }
   timerMilliseconds.innerHTML = millisecondsText; //to get the last 3 digits
 
